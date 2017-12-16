471b3a8a66b10ada707696fe72cdd8c9ef46d22c
dr1zzle <luke12sky@web.de>
2017-12-16 12:00:32 +0100

    Revert "Refactored sytling."
    This reverts commit ea360aa9d8df0585e503412fccbf0601dd445bf3.

--- RSSBot/RssWebhookEntity.cs
+++ RSSBot/RssWebhookEntity.cs
@@ -4,10 +4,16 @@ namespace RSSBot
 {
     internal class RssWebhookEntity
     {
+        public RssWebhookEntity()
+        {
+            LastFeeds = new List<string>();
+            SentFeeds = new List<string>();
+        }
+
         public string Url { get; set; }
         public string Webhook { get; set; }
-        public IList<string> LastFeeds { get; set; } = new List<string>();
-        public IList<string> SentFeeds { get; set; } = new List<string>();
+        public IList<string> LastFeeds { get; set; }
+        public IList<string> SentFeeds { get; set; }
         public DiscordWebhookMessage WebhookMessageTemplate { get; set; }
     }
 }
