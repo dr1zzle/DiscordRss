7494f9a0870a28151fbe8ba5e35470257a1a7e8e
dr1zzle <luke12sky@web.de>
2017-12-22 23:57:51 +0100

    Refactored code.

--- RSSBot/LoggingLocations.cs
+++ RSSBot/LoggingLocations.cs
@@ -2,6 +2,7 @@
 {
     internal static class LoggingLocations
     {
+        internal const string Start = "Logging/Start.txt";
         internal const string SendMessage = "Logging/SendMsgLog.Txt";
         internal const string GetFeed = "Logging/GetRssLog.Txt";
     }
